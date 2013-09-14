c5e943a24a39507f69f911bdbc337018741435fe
BeaVe <donbeave@gmail.com>
2013-09-14 14:47:42 +0800

    Removed forks config.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,13 +2,6 @@ grails.project.class.dir = 'target/classes'
 grails.project.test.class.dir = 'target/test-classes'
 grails.project.test.reports.dir = 'target/test-reports'
 
-grails.project.fork = [
-    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon: true],
-    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve: false],
-    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve: false],
-    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
-]
-
 //grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
   inherits('global') {
